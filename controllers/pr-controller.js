9ef1381ca969bd5be3e5e370bb3b8fa58b1b4967
EnvyL27 <farrel.ahmad.zaidan@gmail.com>
2023-11-14 16:48:07 +0700

    add condition if not input image

--- controllers/pr-controller.js
+++ controllers/pr-controller.js
@@ -11,27 +11,36 @@ module.exports = {
     },
     store: async (req, res) => {
         try {
-          const document = await Pr_monitoring.create({
-            req_date: req.body.req_date,
-            item_desc_img: `/images/${req.file.filename}`,
-            item_desc: req.body.item_desc,
-            pic: req.body.pic,
-            section: req.body.section,
-            area: req.body.area,
-            due_date: req.body.due_date,
-            reason: req.body.reason,
-            pr_number: req.body.pr_number,
-            v_name: req.body.v_name,
-            v_value: req.body.v_value,
-            v2_name: req.body.v2_name,
-            v2_value: req.body.v2_value,
-            bidding: req.body.bidding,
-            keterangan: req.body.keterangan,
-          });
-          res.status(200).json(document);
+            let itemDescImgPath = ''; // Initialize with an empty string
+    
+            // Check if an image file was uploaded
+            if (req.file) {
+                itemDescImgPath = `/images/${req.file.filename}`;
+            }
+    
+            const document = await Pr_monitoring.create({
+                req_date: req.body.req_date,
+                item_desc_img: itemDescImgPath,
+                item_desc: req.body.item_desc,
+                pic: req.body.pic,
+                section: req.body.section,
+                area: req.body.area,
+                due_date: req.body.due_date,
+                reason: req.body.reason,
+                pr_number: req.body.pr_number,
+                v_name: req.body.v_name,
+                v_value: req.body.v_value,
+                v2_name: req.body.v2_name,
+                v2_value: req.body.v2_value,
+                bidding: req.body.bidding,
+                keterangan: req.body.keterangan,
+            });
+    
+            res.status(200).json(document);
         } catch (error) {
             console.error(error)
-          res.status(500).json(error);
+            res.status(500).json(error);
         }
-      },
+    },
+    
 }

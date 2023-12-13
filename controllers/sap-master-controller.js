8caf33909bf2b8100ee9ed39f779b78c1eda7c4a
EnvyL27 <farrel.ahmad.zaidan@gmail.com>
2023-12-13 10:20:12 +0700

    add router and controller for qpgt

--- controllers/sap-master-controller.js
+++ controllers/sap-master-controller.js
@@ -1,5 +1,6 @@
 const { log } = require('console');
 const { iflotx } = require('../models/sap_master/ifloxt')
+const { qpgt } = require('../models/sap_master/qpgt')
 const fs = require('fs')
 
 module.exports = {
@@ -22,6 +23,24 @@ module.exports = {
             res.status(200).json(pr);
         } catch (e) {
             console.log(e)
+            res.status(500).json(e)
+        }
+    },
+    qpgtIndex: async (req, res) => {
+        try {
+            const pr = await qpgt.findAll({attributes: ['KATALOGART', 'CODEGRUPPE', 'KURZTEXT']});
+            console.log(pr)
+            res.status(200).json(pr);
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    },
+    qpctIndex: async (req, res) => {
+        try {
+            const pr = await qpgt.findAll({attributes: ['CODE', 'CODEGRUPPE', 'KURZTEXT']});
+            console.log(pr)
+            res.status(200).json(pr);
+        } catch (e) {
             res.status(500).json(e)
         }
     },
--- routes/sap-router.js
+++ routes/sap-router.js
@@ -3,10 +3,12 @@ var router = express.Router();
 const moment = require("moment");
 const path = require("path");
 const multer = require("multer");
-var funcLocController = require("../controllers/func-loc-controller")
+var funcLocController = require("../controllers/sap-master-controller")
 
 /* GET home page. */
 router.get('/iflotx', funcLocController.index);
+router.get('/qpgt', funcLocController.qpgtIndex);
+router.get('/qpct', funcLocController.qpctIndex);
 router.get('/iflotx/:id', funcLocController.findById);
 
 module.exports = router;

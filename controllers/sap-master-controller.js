5be0cb322e4aebae1cbbec0e2e1bad4048c8a10e
EnvyL27 <farrel.ahmad.zaidan@gmail.com>
2023-12-13 16:21:04 +0700

    update sap master controller

--- controllers/sap-master-controller.js
+++ controllers/sap-master-controller.js
@@ -1,6 +1,7 @@
 const { log } = require('console');
 const { iflotx } = require('../models/sap_master/ifloxt')
 const { qpgt } = require('../models/sap_master/qpgt')
+const { qpct } = require('../models/sap_master/qpct')
 const fs = require('fs')
 
 module.exports = {
@@ -37,11 +38,11 @@ module.exports = {
     },
     qpctIndex: async (req, res) => {
         try {
-            const pr = await qpgt.findAll({attributes: ['CODE', 'CODEGRUPPE', 'KURZTEXT']});
+            const pr = await qpct.findAll({attributes: ['KATALOGART', 'CODE', 'CODEGRUPPE', 'KURZTEXT']});
             console.log(pr)
             res.status(200).json(pr);
         } catch (e) {
             res.status(500).json(e)
         }
     },
-}
+} 
